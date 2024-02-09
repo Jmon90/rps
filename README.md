cd016946fdacee31981676030e221b8876fdf580
Jmon90 <monahan5895@gmail.com>
2024-02-08 18:04:34 -0800

    added function getComputerChoice and it works!

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ Project: Rock Paper Scissors
 2/8/24
 
 Console version only!
+
+
+create func getComputerChoice randomly picks rock paper or scissors
+
+function the plays one round
+ takes two parameters playerSelection and computerSelection and return string that declares winner or tie of round
+ 
+ make case insensitive
+
+ 
--- rps.js
+++ rps.js
@@ -1,1 +1,38 @@
 console.log("Hello World!");
+
+// Function to choose rock paper or scissors
+function getComputerChoice(cchoice) {
+    // gets random index value
+const randomItem = Math.floor(Math.random() * cchoice.length);
+// gets random item
+const item = cchoice[randomItem];
+
+return item;
+}
+
+const array = ['Rock', 'Paper', 'Scissors'];
+const result = getComputerChoice(array);
+
+
+console.log(result);
+
+function playGame(){
+
+
+
+
+
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+  
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+  
+
+
+
+}
+
