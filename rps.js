ff98e76c962d2fdede21789f4a8e01cbaac36ec8
Jmon90 <monahan5895@gmail.com>
2024-02-11 14:44:26 -0800

    Finishedgit add .

--- rps.js
+++ rps.js
@@ -1,54 +1,71 @@
-console.log("Hello World!");
+console.log("RPS! Go!");
+
+let choices = ['rock', 'paper', 'scissors'];
 
 // Function to choose rock paper or scissors
-function getComputerChoice(cchoice) {
-    const myArray = ['rock', 'paper', 'scissors'];
 
-let randomItem = myArray[Math.floor(Math.random() * myArray.length)];
+function getComputerChoice() {
 
-
-return randomItem;
+return choices[(Math.floor(Math.random() * choices.length))];
 }
+getComputerChoice();
+const computerSelection = getComputerChoice();
 
 
 
-console.log();
-// It Works!!
+
+
+let guess = 0;
+let cguess = 0;
 
 
 function playGame(){
-
-   let playerSelection = prompt("Rock, Paper, Scissors").toLowerCase();
-console.log(playerSelection);
-    if (playerSelection === "rock" && getComputerChoice() === "scissors") {
-        alert("You won!");
-    }
+    let playerSelection = prompt("Rock, Paper, Scissors").toLowerCase();
+    console.log(playerSelection);
 
 
-   else if (playerSelection === "paper" && getComputerChoice() === "rock") {
-        alert("You won!");
-    }
-     
-
-   else if (playerSelection === "scissors" && getComputerChoice() === "paper") {
-        alert("You won!");
-    }
-   
-   
-    else {
-    alert("You lost!");
-    }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection = computerSelection){
-        alert('You Win!')
-    }
+
+
+    
+if (playerSelection === computerSelection){
+    alert('Tie!');
+    console.log(computerSelection);
+}
+
+else if (playerSelection === "rock" && computerSelection === "scissors") {
+    alert("You won! Rock beats scissors!");
+    console.log(computerSelection);
+
+    guess += 1;
+}
+
+
+else if (playerSelection === "paper" && computerSelection === "rock") {
+    alert("You won! Paper beats rock!");
+    console.log(computerSelection);
+    guess += 1;
+}
+ 
+
+else if (playerSelection === "scissors" && computerSelection === "paper") {
+    alert("You won! Scissors beats paper!");
+    console.log(computerSelection);
+    guess += 1;
+}
+
+
+else {
+alert("You lost!");
+console.log(computerSelection);
+cguess += 1;
+}
   }
   
- 
-  const computerSelection = getComputerChoice();
+
   console.log(playRound(playerSelection, computerSelection));
-  
+
 
 
 
@@ -58,3 +75,5 @@ playGame();
 playGame();
 playGame();
 playGame();
+
+console.log('Score You: ' + guess + ' Computer: ' + cguess);
